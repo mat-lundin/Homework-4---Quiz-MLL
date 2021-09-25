cfe36da5c19914e321c764196acad52d223d7350
Mat Lundin <matmll12@gmail.com>
2021-09-25 16:19:32 -0500

    initial commit. JS pseudo code and file structure. button added in HTML

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+// WHEN I click the start button - eventlistener
+// THEN a timer starts and I am presented with a question - start timer loop function, hide start button, create divs for box, question, and each answer
+// WHEN I answer a question - eventlistener on container div, event.target on each answer
+// THEN I am presented with another question - call same function to create the question from the array object, next index
+// WHEN I answer a question incorrectly - function to evaluate correct answer and return true/false?
+// THEN time is subtracted from the clock - new timer taking current value and starting a few seconds below that?
+// WHEN all questions are answered or the timer reaches 0 - condition of timer loop plus another function that evaluates all answers given? 
+// THEN the game is over - hide the question boxes and display the start button and score
+// WHEN the game is over
+// THEN I can save my initials and my score - form field to enter initials, save both as separate strings in local storage
